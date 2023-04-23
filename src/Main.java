ec41c231a521368e9be07967f6d130f0e54db36c
Alessio Di Pasquale <38081627+alessiodipasquale@users.noreply.github.com>
2023-04-23 17:44:27 +0200

    Completed Exercise 1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 import org.antlr.v4.runtime.*;
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+    	
+    	//Exercise 1
+    	
         String input = new String(Files.readAllBytes(Paths.get("src/input.txt")));
         CharStream stream = CharStreams.fromString(input);
         SimpLanPlusLexer lexer = new SimpLanPlusLexer(stream);
@@ -19,13 +25,23 @@ public class Main {
             token = lexer.nextToken();
         }
 
-        System.out.println("Numero di errori lessicali: "+ lexerErrors.size());
+        //System.out.println("Numero di errori lessicali: "+ lexerErrors.size());
 
+        File f = new File("out/errors.txt");
+        if(!f.exists()){
+        	f.createNewFile();
+        } else {
+        	f.delete();
+        	f.createNewFile();
+        }
+        
         for (int i = 0; i<lexerErrors.size();i++)
         {
             int errLine = lexerErrors.get(i).getLine();
             String errStr = lexerErrors.get(i).getText();
             int errPos = lexerErrors.get(i).getCharPositionInLine() +1;
-            System.out.printf("Errore %d: Linea %d, carattere numero %d -> %s%n", i + 1, errLine, errPos, errStr);        }
+            String toWrite = "Errore "+i+1+": Linea "+errLine+", carattere numero "+errPos+" -> "+errStr+"\n";
+            Files.write(Paths.get("out/errors.txt"), toWrite.getBytes(), StandardOpenOption.APPEND);
+        }
     }
 }
