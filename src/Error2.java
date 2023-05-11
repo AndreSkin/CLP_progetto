3dbaf868d8189d6c0fe92a996e80ef39eed077bf
Andrea <80905987+AndreSkin@users.noreply.github.com>
2023-05-11 22:14:55 +0200

    Create Error2.java

---
+++ src/Error2.java
@@ -0,0 +1,9 @@
+public class Error2 extends RuntimeException
+{
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public String getMessage() {
+        return "\nErrore";
+    }
+}
